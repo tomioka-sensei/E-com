bb8b3a8f50366f4926b32423575df1023f0948a1
prateek tiwari <tiwariprateek1976@gmail.com>
2024-03-25 02:26:05 +0530

    mui used

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/homePage';
+import Login from './pages/login';
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>}>
+        <Route path="/" element={<Login/>}>
           {/* <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
